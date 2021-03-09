50e3dcf287d2790b0c848c9090b9c18d3467f82c
PGluk <glukhov.p@gmail.com>
2021-03-09 19:50:36 +0500

    ADDED: Follow, Group, Post and Comment serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
+from rest_framework.validators import ValidationError
 
-from .models import Comment, Post
+from .models import Comment, Post, Follow, Group, User
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
+    group = serializers.ReadOnlyField(source='group.title')
 
     class Meta:
-        fields = ('id', 'text', 'author', 'pub_date')
+        fields = '__all__'
         model = Post
 
 
@@ -15,5 +17,35 @@ class CommentSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
-        fields = ('id', 'author', 'post', 'text', 'created')
+        fields = '__all__'
         model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Group
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(slug_field='username', read_only=True)
+    following = serializers.SlugRelatedField(slug_field='username',
+                                             queryset=User.objects.all())
+
+    def validate(self, data):
+        user = self.context['request'].user
+        following = data.get('following')
+
+        if user == following:
+            raise ValidationError('You cannot follow to yourself')
+
+        if Follow.objects.filter(user__username=user,
+                                 following__username=following).exists():
+            raise ValidationError(
+                f'You already followed to {data.get("following")}')
+
+        return data
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
