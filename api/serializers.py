2918443e2e28cdf926ad7c5b98ad8446f9b97110
PGluk <glukhov.p@gmail.com>
2021-03-09 21:34:24 +0500

    CHANGED: user's validator in Meta class changed to UniqueTogetherValidator

--- api/serializers.py
+++ api/serializers.py
@@ -1,36 +1,50 @@
-from rest_framework import serializers
-from rest_framework.validators import ValidationError
+from rest_framework.serializers import (
+    ModelSerializer,
+    SlugRelatedField,
+    ReadOnlyField,
+    CurrentUserDefault,
 
-from .models import Comment, Post, Follow, Group, User
+)
+from rest_framework.validators import ValidationError, UniqueTogetherValidator
+
+from .models import (
+    Comment,
+    Post,
+    Follow,
+    Group,
+    User,
+)
 
 
-class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
-    group = serializers.ReadOnlyField(source='group.title')
+class PostSerializer(ModelSerializer):
+    author = ReadOnlyField(source='author.username')
+    group = ReadOnlyField(source='group.title')
 
     class Meta:
         fields = '__all__'
         model = Post
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+class CommentSerializer(ModelSerializer):
+    author = ReadOnlyField(source='author.username')
 
     class Meta:
         fields = '__all__'
         model = Comment
 
 
-class GroupSerializer(serializers.ModelSerializer):
+class GroupSerializer(ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Group
 
 
-class FollowSerializer(serializers.ModelSerializer):
-    user = serializers.SlugRelatedField(slug_field='username', read_only=True)
-    following = serializers.SlugRelatedField(slug_field='username',
-                                             queryset=User.objects.all())
+class FollowSerializer(ModelSerializer):
+    user = SlugRelatedField(slug_field='username',
+                            read_only=True,
+                            default=CurrentUserDefault())
+    following = SlugRelatedField(slug_field='username',
+                                 queryset=User.objects.all())
 
     def validate(self, data):
         user = self.context['request'].user
@@ -38,14 +52,15 @@ class FollowSerializer(serializers.ModelSerializer):
 
         if user == following:
             raise ValidationError('You cannot follow to yourself')
-
-        if Follow.objects.filter(user__username=user,
-                                 following__username=following).exists():
-            raise ValidationError(
-                f'You already followed to {data.get("following")}')
-
         return data
 
     class Meta:
-        fields = '__all__'
+        fields = ('user', 'following')
         model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following'],
+                message='you already followed to the user'
+            )
+        ]
